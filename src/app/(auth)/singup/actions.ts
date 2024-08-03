fbc953b9f4c101e2cddaf5d7caeec8d644cbc9d6
AbtahiHasan <anikidpersonal@gmail.com>
2024-08-03 16:39:10 +0600

    sign up server action done

---
+++ src/app/(auth)/singup/actions.ts
@@ -0,0 +1,81 @@
+"use server";
+import { hash } from "@node-rs/argon2";
+
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import { generateIdFromEntropySize } from "lucia";
+import prisma from "@/lib/prisma";
+import { lucia } from "@/auth";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function name(
+  credentials: SignUpValues,
+): Promise<{ error: string }> {
+  try {
+    const { username, password, email } = signUpSchema.parse(credentials);
+
+    const hashPassword = await hash(password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+
+    const userId = generateIdFromEntropySize(10);
+
+    const existingUsername = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingUsername) {
+      return {
+        error: "username already taken",
+      };
+    }
+    const existingEmail = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingEmail) {
+      return {
+        error: "email already taken",
+      };
+    }
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        username,
+        displayName: username,
+        email,
+        password: hashPassword,
+      },
+    });
+
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+
+    return redirect("/");
+  } catch (error) {
+    console.error(error);
+    return {
+      error: "something went wrong, please try again",
+    };
+  }
+}
